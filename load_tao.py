7cc20e499cdfc7ababda7540b415adbe6f2dc47c
dakoteus <dakoteus2117@gmail.com>
2021-10-16 21:36:45 -0400

    adding json and passage loader

---
+++ load_tao.py
@@ -0,0 +1,14 @@
+import json
+from random import randint
+tjson = './tao.json'
+tao = {}
+
+def load():
+    with open(tjson, 'r+') as f:
+        testao = json.load(f)
+    return testao
+
+tao = load()
+keys = list(tao.keys())
+i = randint(0,len(keys)-1)
+print(tao[keys[i]])
